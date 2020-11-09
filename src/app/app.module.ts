7f65c2b15575993fc59cd9ee3e7258249f4e465b
guilhermemoc <adm.gms@gmail.com>
2020-11-09 19:51:28 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListagemComponent } from './listagem/listagem.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListagemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HttpClientModule
   ],
+  exports: [ CommonModule, ReactiveFormsModule, RouterModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
